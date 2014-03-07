4819591065dc170870ff649f0679a0017d2f3bed
Roberto Lapuente <topo@asustin.net>
2014-03-07 02:39:39 -0600

    Add prototype for next-gen camera

--- src/camera.py
+++ src/camera.py
@@ -28,9 +28,18 @@ from global_variables import *
 
 # ==============================================================================
 def follow_char(character):
+	""" This function moves the camera so that it will always stay on top of the
+	object given in the 'character' parameter. Usually used to follow the player
+	but this is also useful when making in game cinematics during the campaing"""
 	newpos = (character.rect[0], character.rect[1])
 	distance = geometry.delta(screen_center, newpos)
 	if distance != (0,0):
 		distance = (-distance[0], -distance[1])
 		for spr in allsprites.sprites():
 			spr.rect.move_ip(distance)
+
+# ==============================================================================
+def fancy_new_cam(character):
+	""" Similar to the 'follow_char' but this camera should work like the cam in
+	'Zelda: Minish Cap' when you reach the end of a room, the camera stays still and the
+	char gets to move all the way to the end of the screen"""
