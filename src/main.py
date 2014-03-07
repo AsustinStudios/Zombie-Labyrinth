AsustinStudios/Zombie-Labyrinth

commit 3c50a67d480f3a64454fef4e50f5a908061a7cb9

Change default srpite look to SOUTH & add zombie look to topo

src/main.py
@@ -72,6 +72,7 @@ def main_loop():
 
 	# Prepare Game Objects
 	objects_group = pygame.sprite.Group()
+
 	topo = Human((600,300), 'topo')
 	topo.weapons[0] = Cold_weapon(40, 50)
 	topo.weapons[1] = Firearm(40)
@@ -109,6 +110,7 @@ def main_loop():
 
 		pos = pygame.mouse.get_pos()	# TODO: Human Object must be bindable to
 		topo.look(pos)					# a device or a target must be specifyable.
+		zombie.look((topo.rect[0], topo.rect[1]))
 
 		# Mantener la cámara en el centro del nivel
 		camera_effects(topo)

src/living_being.py
@@ -41,7 +41,7 @@ class Living_being(Game_object):
 	def __init__(self, start_location=(50, 50), sprite_prefix='living_being', object_type='LIVING'):
 		Game_object.__init__(self, start_location, sprite_prefix, object_type)
 		self.collision_group = None
-		self.direction = NORTH
+		self.direction = SOUTH
 
 		# ======================================================================
 		""" Permanent Stats"""

commit 7436e54a384d3cd285622f39dc79e8c2c51cdd33

Break Zombie look introduced on 3c50a67. Also modularized main loop
Start separating the main loop in general modules to be able to use the
same main loop for all the missions.

Break the functionality introduced in 3c50a67 that allow the zombies to
look at the player.

TODO: Fix the bug but on the AI file.

3c50a67

src/main.py
@@ -38,53 +38,27 @@ from human import Human
 from zombie import Zombie
 from game_object import Game_object
 from weapons import *
+from ai import *
+
+from pygame.examples import movieplayer
 
 # ==============================================================================
 def main():
 	global preferences
 	preferences = process_cli_options()
 
-	status = main_loop()
+	screen, background = prepare_engine()
+
+	player = resources.load_level('Prueba')
+	ai = main_ai
+	camera = camera_effects
+	status = main_loop(screen, background, player, ai, camera)
 
 	return status
 
 # ==============================================================================
-def main_loop():
-	# Initialize Everything
-	pygame.init()
-	screen = pygame.display.set_mode((1280, 720))
-	pygame.display.set_caption('Zombie Labyrinth')
-	pygame.mouse.set_visible(True)
-
-	# Create Background
-	background = pygame.Surface(screen.get_size())
-	background = background.convert()
-	background.fill((205, 133, 63))
-
-	# Text
+def main_loop(screen, background, player, ai, camera):
 	font = pygame.font.Font(None, 20)
-	text = font.render('FPS:   | Life: ', 1, (10, 10, 10))
-	textpos = text.get_rect(centerx=background.get_width()/2)
-	background.blit(text, textpos)
-
-	# Display Background
-	screen.blit(background, (0, 0))
-	pygame.display.flip()
-
-	# Prepare Game Objects
-
-	resources.load_level('nivel')
-
-	topo = Human((600,300), 'topo')
-	topo.weapons[0] = Cold_weapon(40, 50)
-	topo.weapons[1] = Firearm(40)
-	topo.collision_group = objects_group
-
-	zombies = (Zombie((400,400)),Zombie((200,200)),Zombie((600,600)),Zombie((600,400)),Zombie((600,200)))
-	objects_group.add(zombies)
-
-	allsprites.add(zombies, topo)
-	allsprites.add(topo)
 
 	clock = pygame.time.Clock()
 	pygame.key.set_repeat(10, 20)
@@ -102,31 +76,31 @@ def main_loop():
 					return 0
 				elif event.key in (K_RIGHT, K_LEFT, K_UP, K_DOWN, K_d, K_a, K_w, K_s):
 					keys = pygame.key.get_pressed()
-					topo.move(input.get_directions(keys))
+					player.move(input.get_directions(keys))
 				elif event.key == K_F1:
 					screen = pygame.display.set_mode((1920, 1080))
 				elif event.key == K_F2:
 					pygame.display.toggle_fullscreen()
 			elif event.type == MOUSEBUTTONDOWN:
 				if event.button == 1:
-					topo.attack(0)
+					player.attack(0)
 				elif event.button == 3:
-					topo.attack(1)
+					player.attack(1)
 
 		pos = pygame.mouse.get_pos()	# TODO: Human Object must be bindable to
-		topo.look(pos)					# a device or a target must be specifyable.
-		for zombie in zombies:
-			zombie.look((topo.rect[0], topo.rect[1]))
+		player.look(pos)					# a device or a target must be specifyable.
+
+		ai()
 
 		# Mantener la cámara en el centro del nivel
-		camera_effects(topo)
+		camera(player)
 
 		# Update all the sprites
 		allsprites.update()
 
 		# Display Current FPS
 		background.fill((205, 133, 63))
-		text = font.render('FPS: %i | Life: %i' % (fps, topo.life), 1, (10, 10, 10))
+		text = font.render('FPS: %i | Life: %i' % (fps, player.life), 1, (10, 10, 10))
 		textpos = text.get_rect(centerx=background.get_width()/2)
 		background.blit(text, textpos)
 
@@ -173,6 +147,24 @@ def process_cli_options():
 	#preferences.thing = new_thing
 
 	return preferences
+
+# ==============================================================================
+def prepare_engine():
+	# Initialize the engine, screen && background
+	pygame.init() # Initialize Engine
+	pygame.display.set_caption('Zombie Labyrinth')
+	pygame.mouse.set_visible(True)
+
+	# Play intro video
+	#movieplayer.main('/home/roberto/Videos/Banned Commercials - Microsoft Office XP (Banned Too Sexy).mpeg')
+
+	# Initialize screen && drawing area
+	screen = pygame.display.set_mode((1280, 720))
+	background = pygame.Surface(screen.get_size())
+	background = background.convert()
+	background.fill((205, 133, 63))
+
+	return screen, background
 
 # ==============================================================================
 if __name__ == '__main__':

src/resources.py
@@ -30,6 +30,9 @@ from pygame.locals import *
 
 from global_variables import *
 from game_object import Game_object
+from weapons import *
+from human import Human
+from zombie import Zombie
 
 # ==============================================================================
 def load_image(name, colorkey=None):
@@ -66,7 +69,7 @@ def load_sound(name):
 	return sound
 
 # ==============================================================================
-def load_level(name):
+def load_map(name):
 	""" This Function loads all the level construction, create the objects and
 	get them in their respective groups."""
 	name = '%s.lvl' % name
@@ -95,3 +98,23 @@ def load_level(name):
 			j += 1
 		line = file.readline()
 		i += 1
+
+# ==============================================================================
+def load_level(name):
+	""" This Should load missions from a text file or have a function for each
+	mission or level. However, by the time being it just loads the default map
+	and settings."""
+	load_map('nivel')
+
+	topo = Human((600,300), 'topo')
+	topo.weapons[0] = Cold_weapon(40, 50)
+	topo.weapons[1] = Firearm(40)
+	topo.collision_group = objects_group
+
+	zombies = (Zombie((400,400)), Zombie((200,200)), Zombie((600,600)),
+				Zombie((600,400)), Zombie((600,200)), Zombie((900,300)))
+	objects_group.add(zombies)
+
+	allsprites.add(zombies, topo)
+
+	return topo